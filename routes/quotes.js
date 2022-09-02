5d6e863fedef05a63d5244dc0c4d41c40abffd57
J05hL <josh.langford@hotmail.co.uk>
2022-09-02 15:01:09 +0100

    working post

---
+++ routes/quotes.js
@@ -0,0 +1,27 @@
+import express from "express";
+const router = express.Router();
+
+import {buildQuoteForArray} from '../calculations/mainCalcFunc.js'
+
+console.log(buildQuoteForArray)
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  console.log(`Hi`)
+  res.json({ message: "I wish we had some information to give you ☹️" });
+});
+
+export default router;
+
+router.post("/", async function (req, res) {
+  const pets = await req.body;
+  console.log(pets)
+  const quote = buildQuoteForArray(pets)
+  console.log(buildQuoteForArray(pets))
+  try {
+    res.status(201).json({success: true, payload: `£${quote}`});
+  } catch (err) {
+    res.status(400).json({success: false, message: err.message });
+  }
+});
+
