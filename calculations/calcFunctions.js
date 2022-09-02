aa4f414ffa9c35481d4231a21da6a69ad7e20ac5
J05hL <josh.langford@hotmail.co.uk>
2022-09-02 10:56:26 +0100

    1st commit

---
+++ calculations/calcFunctions.js
@@ -0,0 +1,22 @@
+// - Take a base price of £120 / year (£10 a month)
+// - Add an increase of 5% to the price for each year up to 5 and 10% for each year past that up to 10
+// - Apply a 10% lower price to 3 breeds
+// - Apply a 15% higher price to 3 address areas
+// - Apply the multi-pet discount at 10% for each pet when 2 or more pets are included
+// - Validation
+
+// Array[
+// {	Pet: String
+//   Year Age:  Integer
+//   Month Age: Integer
+//   Breed : String
+//   Address : String
+//   }, 
+//   {	Pet: cat || dog
+//   Year Age:  1-20yrs
+//   Month Age: 1-12months
+//   Breed : search
+//   Address : search
+//   }
+//   ]
+  
