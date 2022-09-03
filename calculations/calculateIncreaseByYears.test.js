8081a956bb1dacd38f4ca6286ac7b083dc3ed654
J05hL <josh.langford@hotmail.co.uk>
2022-09-03 23:24:25 +0100

    changes to equal the required values and also added one more test to cover 0-5 range when non zero - all green

--- calculations/calculateIncreaseByYears.test.js
+++ calculations/calculateIncreaseByYears.test.js
@@ -7,20 +7,26 @@ describe('check that calculateIncreaseByYears returns the correct increase to qu
     console.log(`1st test`, Date())
     test(`age 0 returns a 0 = 0% increase`,
       () => {
-        expect(calculateIncreaseByYears(0)).toEqual(0.1)
+        expect(calculateIncreaseByYears(0)).toEqual(0)
       }
     )
     console.log(`2nd test`, Date())
     test(`age 10 returns a 0.75 = 75% increase`,
       () => {
-        expect(calculateIncreaseByYears(10)).toEqual(0.80)
+        expect(calculateIncreaseByYears(10)).toEqual(0.75)
       }
     )
     console.log(`3rd test`, Date())
     test(`age 7 returns a 0.45 = 45% increase`,
       () => {
-        expect(calculateIncreaseByYears(7)).toEqual(0.5)
+        expect(calculateIncreaseByYears(7)).toEqual(0.45)
       }
-    )  
+    )
+    console.log(`4th test`, Date())
+    test(`age 1 returns a 0.05 = 5% increase`,
+      () => {
+        expect(calculateIncreaseByYears(1)).toEqual(0.05)
+      }
+    )   
   }
 )
