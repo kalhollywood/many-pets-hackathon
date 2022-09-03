d985ae18b16e9e0b75c26c4fe6e2147d807cdf6e
J05hL <josh.langford@hotmail.co.uk>
2022-09-03 23:28:29 +0100

    added dummy tests for now for the two remaining funcs which will fail as a reminder but want to get at least one validation working on backend before i call it a night!

---
+++ calculations/mainCalcFunc.test.js
@@ -0,0 +1,14 @@
+import {describe, test, expect} from "@jest/globals";
+
+
+
+describe('dummy test for now but write more asap', 
+  ()=> {
+    console.log(`1st test`, Date())
+    test(`dummy test`,
+      () => {
+        expect(0).toEqual(1)
+      }
+    )
+  }
+)
